a21e2a94516993e4c7abbe3fb728cbb8e830add9
Ridhi004 <rridhi1_be23@thapar.edu>
2026-09-13 08:04:20 +0530

    Add: validate-parentheses - submission-1

---
+++ Data Structures & Algorithms/validate-parentheses/submission-1.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        std::stack<char>st;
+        for(char c:s){
+            if(c=='(')st.push(')');
+            else if(c=='{')st.push('}');
+            else if(c=='[')st.push(']');
+            else if(st.empty()|| st.top()!=c)return false;
+            else st.pop();
+
+        }
+        return st.empty();
+        
+    }
+};
